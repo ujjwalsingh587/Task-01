53273fe62a0d6fec267d66ae296c4b8a670339ce
ujjwalsingh587 <ujjwalsinghbaghel587@gmail.com>
2025-12-09 21:30:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// simple interaction: show message when button is clicked
+const btn = document.getElementById("msgBtn");
+
+btn.addEventListener("click", function () {
+  alert("Hello! This is my first JS interaction ✅");
+});
